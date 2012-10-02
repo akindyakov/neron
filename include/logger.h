955114479becf02d26cc01ed241fd8f2cbc9b03e
AKindyakov <akindyakov@gmail.com>
2012-10-02 09:32:32 +0400

    erase swp

---
+++ include/logger.h
@@ -0,0 +1,22 @@
+/**
+ ** autor   Alexander K
+ ** date    1.10.2012.21.51
+ ** BRIEF   project logger for write specific type and log to console and to
+ **         log-file
+*/
+
+#ifndef LOGGER_G_H
+#define LOGGER_G_H
+//=============================================================================
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+//=============================================================================
+//=============================================================================
+
+namespace Log
+{
+   
+}
+#endif // LOGGER_G_H
