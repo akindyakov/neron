51089117a62c11ec07c3cd99a3779e54d6c47e65
AKindyakov <akindyakov@gmail.com>
2012-12-13 11:53:29 +0400

    add header for prob point

---
+++ include/probabilistic_point.h
@@ -0,0 +1,16 @@
+/**
+ ** autor   Alexander K
+ ** date    13.12.2012.11.49
+ ** BRIEF   points for probabilistic estimate sensors measuring
+*/
+
+#ifndef _PROBABILISTIC_POIT_H_
+#define _PROBABILISTIC_POIT_H_
+//=============================================================================
+//=============================================================================
+//=============================================================================
+namespace ProbPoint
+{
+   
+}
+#endif //_PROBABILISTIC_POIT_H_
