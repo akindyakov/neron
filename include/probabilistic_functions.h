e6982dae84d165aaf62542bff6bed7c8908885b3
AKindyakov <akindyakov@gmail.com>
2013-01-12 22:02:53 +0400

    add template for probabilistic law \n carrout math util from src/util to src/util/math/

--- include/probabilistic_functions.h
+++ include/probabilistic_functions.h
@@ -13,26 +13,46 @@
 
 namespace Math
 {
+   template class Vt
    class I_Prob_distrib
    {
    public:
-      virtual float getIntencity(float arg);
-      virtual float getIntegral(float start, float finish);
+      virtual Vt getIntencity(Vt arg);
+      virtual Vt getIntegral(Vt start, Vt finish);
+      
+      Vt m_expectation;
    };
    
-   class Normal_distrib: public I_Prob_distribution
+   template class Vt
+   class Normal_distrib: public I_Prob_distribution<Vt>
    {
    public:
-   Normal_distrib();
-   Normal_distrib();
+      Normal_distrib(Vt diviation, Vt expectation);
+      
    private:
+      Vt m_deviation;
    };
    
-   class Normal_linearised_distrib: public I_Prob_distribution
+   template class Vt
+   class Normal_linearised_distrib: public I_Prob_distribution<Vt>
    {
    public:
-    Normal_linearised_distrib(std::vector<float> coeff);
+      Normal_linearised_distrib(Vt diviation, Vt expectation);
+      
    private:
+      static const Vt m_coeff[256];
    };
+   
+   Normal_linearised_distrib::m_coeff = 
+   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+
 } //end namespace Math
+
 #endif // PROBABILISTIC_LOW_H
