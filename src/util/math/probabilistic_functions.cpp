2b7ff54625aad71769c4aa5bef9bee6075fe72e8
AKindyakov <akindyakov@gmail.com>
2013-01-15 00:21:13 +0400

    implementation getDensity for normal law

--- src/util/math/probabilistic_functions.cpp
+++ src/util/math/probabilistic_functions.cpp
@@ -10,6 +10,9 @@
 
 namespace M = Math;
 
+// TODO carry out this constants to math header
+double const InverseSqrtTwoPi = 0.39894228040143; // 1/(sqrt(2*pi))
+
 template class Vt
 M::Normal_distrib(Vt diviation, Vt expectation)
    :  m_diviation(diviation)
@@ -20,13 +23,15 @@ M::Normal_distrib(Vt diviation, Vt expectation)
 template class Vt
 Vt M::Normal_distrib::getDencity(Vt arg)
 {
-   
+   Vt cmp_temp = (arg - m_expectation) / m_diviation;
+   cmp_temp *= cmp_temp/2;
+   return ( InverseTwoPi/(m_deviation) ) * exp( -cmp_temp);
 }
 
 template class Vt
 Vt M::Normal_distrib::getIntegral(Vt start, Vt finish)
 {
-   
+  //think about it 
 }
 
 template class Vt
