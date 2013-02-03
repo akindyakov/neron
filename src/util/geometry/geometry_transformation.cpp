08295213087f5bce61cd702400b3eb2bc11724a8
AKindyakov <akindyakov@gmail.com>
2013-02-04 00:08:17 +0400

    add comment to create shadow

--- src/util/geometry/geometry_transformation.cpp
+++ src/util/geometry/geometry_transformation.cpp
@@ -19,25 +19,48 @@ void G::createShadow  ( const Convex_contour& src_contour,
                         std::vector< Convex_contour >* out_contour,
                         const std::vector< Reduced_vector >& start_points )
 {
-  // using namespace std;
-  // using namespace Geometry;
+   using namespace std;
+   using namespace Geometry;
    
-  // size_type out_size = start_point.size();
    out_contour->resize(0); // correct me if it will be need !
+   
+   /*      {  start.x + shadow.x + k*B = src.x
+    *     {   start.y + shadow.y - k*A = src.y
+    *      {  shadow.x * src.y = shadow.y * src.x
+    *
+    *                                                                   2
+    *                src.x (start.y - src.y) B + src.x start.x A - src.x  A
+    *   shadow.x = - ---------------------------------------------------------
+    *                       src.y B + src.x A
+    *
+    *                                      2
+    *                (src.y start.y - src.y ) B + src.y start.x A - src.x src.y A
+    *   shadow.y = - -------------------------------------------------------------
+    *                       src.y B - src.x A
+    *
+    *          src.y start.x - src.x start.y
+    *   k = - ------------------------------
+    *                 src.y B + src.x A
+    */
+    
    //vector< float > distance;
    
    float prev_vect_proud;
    float curr_vect_proud;
+   
+   vector< Reduced_vector > prevShadowShifts(start_points);
+   vector< Reduced_vector > nextBisector(start_points.size());
+   
    // it are neded to create start points for out contours
-/*   for ( vector< Reduced_vector >::const_iterator start_pts_it = start_points.begin();
+   for ( vector< Reduced_vector >::const_iterator start_pts_it = start_points.begin();
          start_pts_it != start_points.end();
          ++start_pts_it)
    {
       out_contour->push_back( src_contour.m_center + start_pts_it );
-      distance.push_back();
    }
    
-   for ( vector< Reduced_vector >::const_iterator src_it = src_cont.m_vec.begin();
+   for ( vector< Reduced_vector >::const_iterator 
+         src_it = src_cont.m_vec.begin(), src_it = src_cont.m_vec.begin();
          src_it != src_cont.m_vec.end();
          ++src_it )
    {
@@ -45,7 +68,7 @@ void G::createShadow  ( const Convex_contour& src_contour,
             start_pts_it != start_points.end();
             ++start_pts_it)
       {
-         
+         Reduced_vecotor bisector = createBisector(
       }
-   } */
+   }
 }
