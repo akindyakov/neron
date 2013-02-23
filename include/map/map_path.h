3af7ad7156bed8d60c62bdc30e1803d6cff5bb53
AKindyakov <akindyakov@gmail.com>
2013-02-23 23:38:37 +0400

    add path specialized separate file

---
+++ include/map/map_path.h
@@ -0,0 +1,22 @@
+/**
+ ** autor   Alexander K
+ ** date    23.02.2012.23.15
+ ** BRIEF   path util
+*/
+
+#ifndef MAP_PATH_UTIL_H
+#define MAP_PATH_UTIL_H
+//=============================================================================
+#include <vector>
+#include <boost/shared_ptr.hpp>
+//=============================================================================
+#include "include/util/geometry/geometry.h"
+#include "include/map/map.h"
+//=============================================================================
+
+namespace Map
+{
+   
+}
+
+#endif //MAP_PATH_UTIL_H
